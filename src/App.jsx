e88994d5bb26c7bf4a9594b6457b6cbd4923f1f5
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-07-05 18:33:54 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/home";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-orange-400">Hello world!</h1>
+      <Home />
+    </>
+  );
+}
+
+export default App;
