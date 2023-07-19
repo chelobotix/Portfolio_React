4e25147332ed6a359c1235a005a4b2016cf447c8
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-07-19 13:57:03 -0400

    Add caal to Action button on Hero

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import SocialMediaIcons from "../SocialMediaIcons/SocialMediaIcons";
 
 const Contact = () => {
   return (
-    <ul className="w-3/4 flex justify-evenly gap-4 p-1 mt-4 rounded bg-ligth-pink opacity-70">
+    <ul className="w-3/4 flex justify-evenly gap-4 p-1 mt-4 mb-2 rounded bg-ligth-pink opacity-70">
       <SocialMediaIcons color={"#5f43b2"} size={"2rem"} />
     </ul>
   );
