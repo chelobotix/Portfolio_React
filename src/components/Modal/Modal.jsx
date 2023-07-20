535d5d1a404da6ec928e9498bddf3416b7f5a59f
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-07-19 21:39:56 -0400

    Add feature section to modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import { Carousel, IconButton } from "@material-tailwind/react";
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill, BsGithub } from "react-icons/bs";
 import { IconContext } from "react-icons";
+import Button from "../Button/Button";
 
 const Modal = (props) => {
   const { modalConf, setModalConf } = props;
@@ -18,10 +19,10 @@ const Modal = (props) => {
       className={`${modalConf.visible} flex items-start w-screen h-screen overflow-y-auto backdrop-blur-sm bg-white/50 absolute z-30`}
       style={{ top: `${modalConf.positionY}px` }}
     >
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col gap-1 items-center ">
         <p
           onClick={handleModal}
-          className="text-dark-blue text-3xl font-bold self-end p-2 hover:animate-pulse hover:cursor-pointer"
+          className="text-dark-blue text-3xl font-bold self-end px-2 hover:animate-pulse hover:cursor-pointer"
         >
           x
         </p>
@@ -32,7 +33,7 @@ const Modal = (props) => {
           <div></div>
         ) : (
           <Carousel
-            className="h-auto w-10/12 md:w-9/12 md:h-11/12  lg:w-3/5 xl:w-1/2 2xl:w-1/3 rounded-lg m-2"
+            className="h-auto w-11/12 xl:w-2/4 rounded-lg m-2"
             prevArrow={({ handlePrev }) => (
               <IconButton
                 variant="text"
@@ -70,7 +71,7 @@ const Modal = (props) => {
             ))}
           </Carousel>
         )}
-        <div className="flex flex-col items-center bg-dark-blue w-11/12 w-max-11/12 mb-1 opacity-80 rounded-md ring-1 ring-white">
+        <div className="flex flex-col items-center bg-dark-blue w-11/12 lg:w-9/12  mb-1 opacity-80 rounded-md ring-1 ring-white">
           <p className="text-ligth-pink font-semibold">Stack</p>
           <ul className="flex">
             {project.techs.map((tech) => (
@@ -81,20 +82,38 @@ const Modal = (props) => {
               </IconContext.Provider>
             ))}
           </ul>
-          <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+          <hr className="w-48 h-1 mx-auto my-2 md:my-1 bg-gray-100 border-0 rounded  dark:bg-gray-700"></hr>
 
           <p className="text-ligth-pink font-semibold">Description</p>
           <p className="text-sm text-center text-white mx-4">{project.description}</p>
-          <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+          <hr className="w-48 h-1 mx-auto md:my-1 bg-gray-100 border-0 rounded  dark:bg-gray-700"></hr>
 
           <p className="text-ligth-pink font-semibold">Features</p>
-          <ul className="mb-5">
+          <ul className="flex flex-col items-center mb-5">
             {project.features.map((feature) => (
               <li className="text-sm text-white list-disc" key={uuidv4()}>
                 {feature}
               </li>
             ))}
           </ul>
+        </div>
+        <div className="flex gap-2 mt-4 ">
+          <Button
+            name="Live Demo"
+            style={
+              "px-4 py-1 text-sm font-semibold border-2 border-letter-blue bg-letter-blue rounded-3xl text-dark-blue hover:border-white ring-white ring-1 "
+            }
+            liveDemo={project.liveDemo}
+            type={"button"}
+          />
+        </div>
+        <div className="flex flex-col items-center my-3">
+          <a href={project.gitHub} target="_blank">
+            <IconContext.Provider value={{ color: "black", size: "2rem" }}>
+              <BsGithub />
+            </IconContext.Provider>
+          </a>
+          <p className="font-semibold text-sm">GitHub Repo</p>
         </div>
       </div>
     </div>
