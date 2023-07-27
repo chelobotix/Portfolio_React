af54cf8a9fe8643b4ec151fdad8401e2ff063d70
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-07-27 17:29:35 -0400

    Update form style

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,7 +5,8 @@ import Button from "../Button/Button";
 const ContactForm = () => {
   return (
     <section id="contact" className="flex flex-col items-center w-full pb-7 bg-gradient-to-t from-dark-pink to-dark-blue">
-      <h2 className="animateText text-4xl font-bold mt-12 mb-5 lg:text-5xl lg:mb-12">Contact me</h2>
+      <h2 className="animateText text-4xl font-bold mt-12 mb-1 lg:text-5xl">Contact me</h2>
+      <p className="text-xs text-white mb-4">If you like Tesseract like I do, let's talk...</p>
       <form
         className="w-[320px] md:w-1/2 flex flex-col gap-3 p-2 item-center"
         action="https://formspree.io/f/xzbwnlee"
@@ -17,7 +18,7 @@ const ContactForm = () => {
         <Button
           name="Get in touch"
           style={
-            "px-4 py-1 text-sm font-semibold border-2 xl:w-1/2 m-auto border-letter-blue bg-letter-blue rounded-lg text-dark-blue hover:border-white "
+            "px-4 py-1 text-sm font-semibold border-2 xl:w-1/3 m-auto border-letter-blue bg-letter-blue rounded-lg text-dark-blue hover:border-white hover:scale-110 duration-300"
           }
           liveDemo={""}
           modalConf={{}}
