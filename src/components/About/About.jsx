7f9c13e378eff7d9d9f316b8889f619caf2aa739
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-11-21 10:10:17 -0400

    update skills section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,24 +5,25 @@ import chelo from '@/assets/images/chelo.jpg';
 
 const About = () => {
   return (
-    <section id="about" className="flex flex-col gap-3 items-center bg-gradient-to-t from-dark-pink to-dark-blue w-screen p-7">
-      <h2 className="text-white text-4xl font-bold lg:text-5xl">About</h2>
-      <div className="flex flex-col items-center gap-3">
-        <img src={chelo} alt="" className="w-32 h-36 lg:w-44 lg:h-48 border-2 border-white" />
-        <p className="text-white text-center md:text-[18px] leading-loose tracking-wider p-2">
-          Hello! 👋 I&apos;m efficient in crafting robust solutions with Ruby on Rails and React. I thrive in collaborative team environments, always
-          eager to bring innovation to product, a feature, or a website. I love turning ideas into reality. Take a look at some of my projects and
-          experiences. If you find them intriguing and have a coding project on your mind, feel free to reach out. I&apos;m here to bring your ideas
-          to life! 🚀✨
-        </p>
-      </div>
-      <Button
-        name="Get My Resume"
-        style={
-          'px-4 py-1 text-lg font-semibold border-2 border-letter-blue rounded-3xl text-letter-blue hover:bg-letter-blue hover:border-white hover:text-dark-blue m-8'
-        }
-      />
-      <div className="max-w-xl mt-8">
+    <section id="about" className="about">
+      <div className="flex flex-col items-center gap-3 bg-dark-blue bg-opacity-70 p-6">
+        <h2 className="text-white text-4xl font-bold lg:text-5xl">About</h2>
+        <div className="flex flex-col items-center gap-3">
+          <img src={chelo} alt="" className="w-28 h-32 lg:w-40 lg:h-48 border-2 border-white" />
+          <p className="text-white text-center md:text-[18px] leading-loose tracking-wider p-2">
+            Hello! 👋 I&apos;m efficient in crafting robust solutions with Ruby on Rails and React. I thrive in collaborative team environments,
+            always eager to bring innovation to product, a feature, or a website. I love turning ideas into reality. Take a look at some of my
+            projects and experiences. If you find them intriguing and have a coding project on your mind, feel free to reach out. I&apos;m here to
+            bring your ideas to life! 🚀✨
+          </p>
+        </div>
+        <Button
+          name="Get My Resume"
+          style={
+            'px-4 py-1 text-lg font-semibold border-2 border-white rounded-3xl text-white hover:bg-letter-blue hover:border-white hover:text-dark-blue m-8 duration-300'
+          }
+        />
+        <hr className="h-[1px] w-full bg-white border-0 mb-9" />
         <TechAccordion />
       </div>
     </section>
--- src/components/TechAccordion/TechAccordion.jsx
+++ src/components/TechAccordion/TechAccordion.jsx
@@ -1,104 +1,68 @@
-import { v4 as uuidv4 } from "uuid";
-import { Fragment, useState } from "react";
-import { Accordion, AccordionHeader, AccordionBody } from "@material-tailwind/react";
-import techList from "./TechList";
-import { IconContext } from "react-icons";
-
-function Icon({ id, open }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={`${id === open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-    </svg>
-  );
-}
-
-const headerStyle = open === 1 ? "text-white hover:text-letter-blue" : "text-letter-blue hover:text-white";
+import { v4 as uuidv4 } from 'uuid';
+import techList from './TechList';
+import { IconContext } from 'react-icons';
 
 const TechAccordion = () => {
-  const [open, setOpen] = useState(0);
-
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
-  };
-
-  const customAnimation = {
-    mount: { scale: 1 },
-    unmount: { scale: 0.8 },
-  };
-
   return (
-    <Fragment>
-      <Accordion open={open === 1} icon={<Icon id={1} open={open} />} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(1)} className={headerStyle}>
-          Languages
-        </AccordionHeader>
-        <AccordionBody>
-          <ul className="flex justify-center gap-5">
-            {techList.languages.map((tech) => {
-              return (
-                <li key={uuidv4()}>
-                  <IconContext.Provider value={{ color: tech[1], size: "2rem" }}>
-                    <div className="flex flex-col gap-1 items-center">
-                      {tech[0]}
-                      <p className="text-white text-xs">{tech[2]}</p>
-                    </div>
-                  </IconContext.Provider>
-                </li>
-              );
-            })}
-          </ul>
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} icon={<Icon id={2} open={open} />} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(2)} className={headerStyle}>
-          Frameworks & Libraries
-        </AccordionHeader>
-        <AccordionBody>
-          <ul className="flex justify-center flex-wrap gap-5">
-            {techList.frameworks.map((tech) => {
-              return (
-                <li key={uuidv4()}>
-                  <IconContext.Provider value={{ color: tech[1], size: "2rem" }}>
-                    <div className="flex flex-col gap-1 items-center">
-                      {tech[0]}
-                      <p className="text-white text-xs">{tech[2]}</p>
-                    </div>
-                  </IconContext.Provider>
-                </li>
-              );
-            })}
-          </ul>
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} icon={<Icon id={3} open={open} />} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(3)} className={headerStyle}>
-          Skills
-        </AccordionHeader>
-        <AccordionBody>
-          <ul className="flex justify-center flex-wrap gap-5">
-            {techList.skills.map((tech) => {
-              return (
-                <li key={uuidv4()}>
-                  <IconContext.Provider value={{ color: tech[1], size: "2rem" }}>
-                    <div className="flex flex-col gap-1 items-center">
-                      {tech[0]}
-                      <p className="text-white text-xs">{tech[2]}</p>
-                    </div>
-                  </IconContext.Provider>
-                </li>
-              );
-            })}
-          </ul>
-        </AccordionBody>
-      </Accordion>
-    </Fragment>
+    <article className="flex flex-col gap-3 bg-dark-blue bg-opacity-50 rounded-lg p-3">
+      <div className="flex flex-col items-center gap-3">
+        <h3 className="text-white font-light text-lg">Languages</h3>
+        <ul className="flex justify-center gap-5">
+          {techList.languages.map((tech) => {
+            return (
+              <li key={uuidv4()}>
+                <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
+                  <div className="flex flex-col gap-1 items-center">
+                    {tech[0]}
+                    <p className="text-white text-xs">{tech[2]}</p>
+                  </div>
+                </IconContext.Provider>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <hr className="h-[1px] w-full bg-white border-0 my-4" />
+
+      <div className="flex flex-col items-center gap-3">
+        <h3 className="text-white font-light text-lg">Frameworks & Libraries</h3>
+        <ul className="flex justify-center flex-wrap gap-5">
+          {techList.frameworks.map((tech) => {
+            return (
+              <li key={uuidv4()}>
+                <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
+                  <div className="flex flex-col gap-1 items-center">
+                    {tech[0]}
+                    <p className="text-white text-xs">{tech[2]}</p>
+                  </div>
+                </IconContext.Provider>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <hr className="h-[1px] w-full bg-white border-0 my-4" />
+
+      <div className="flex flex-col items-center gap-3">
+        <h3 className="text-white font-light text-lg">Skills</h3>
+        <ul className="flex justify-center flex-wrap gap-5">
+          {techList.skills.map((tech) => {
+            return (
+              <li key={uuidv4()}>
+                <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
+                  <div className="flex flex-col gap-1 items-center">
+                    {tech[0]}
+                    <p className="text-white text-xs">{tech[2]}</p>
+                  </div>
+                </IconContext.Provider>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </article>
   );
 };
 
