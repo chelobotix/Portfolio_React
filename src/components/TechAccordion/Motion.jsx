8c32def0c55f4fd6c2720306fe9b82f4234fac19
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-11-22 10:02:12 -0400

    update tech list

---
+++ src/components/TechAccordion/Motion.jsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+
+const Motion = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ margin: '-50px' }}
+      transition={{
+        duration: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+        scale: {
+          type: 'spring',
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001,
+        },
+      }}
+      className=""
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export { Motion };
--- src/components/TechAccordion/TechAccordion.jsx
+++ src/components/TechAccordion/TechAccordion.jsx
@@ -1,66 +1,73 @@
 import { v4 as uuidv4 } from 'uuid';
 import techList from './TechList';
 import { IconContext } from 'react-icons';
+import { Motion } from './Motion';
 
 const TechAccordion = () => {
   return (
-    <article className="flex flex-col gap-3 bg-dark-blue bg-opacity-50 rounded-lg p-3">
+    <article className="flex flex-col gap-3 bg-dark-blue bg-opacity-50 rounded-lg p-3 ">
       <div className="flex flex-col items-center gap-3">
-        <h3 className="text-white font-light text-lg">Languages</h3>
-        <ul className="flex justify-center gap-5">
-          {techList.languages.map((tech) => {
-            return (
-              <li key={uuidv4()}>
-                <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
-                  <div className="flex flex-col gap-1 items-center">
-                    {tech[0]}
-                    <p className="text-white text-xs">{tech[2]}</p>
-                  </div>
-                </IconContext.Provider>
-              </li>
-            );
-          })}
-        </ul>
+        <h3 className="text-white font-light text-lg underline">Languages</h3>
+        <Motion>
+          <ul className="flex justify-center gap-5">
+            {techList.languages.map((tech) => {
+              return (
+                <li key={uuidv4()}>
+                  <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
+                    <div className="flex flex-col gap-1 items-center">
+                      {tech[0]}
+                      <p className="text-white text-xs">{tech[2]}</p>
+                    </div>
+                  </IconContext.Provider>
+                </li>
+              );
+            })}
+          </ul>
+        </Motion>
       </div>
 
       <hr className="h-[1px] w-full bg-white border-0 my-4" />
 
       <div className="flex flex-col items-center gap-3">
-        <h3 className="text-white font-light text-lg">Frameworks & Libraries</h3>
-        <ul className="flex justify-center flex-wrap gap-5">
-          {techList.frameworks.map((tech) => {
-            return (
-              <li key={uuidv4()}>
-                <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
-                  <div className="flex flex-col gap-1 items-center">
-                    {tech[0]}
-                    <p className="text-white text-xs">{tech[2]}</p>
-                  </div>
-                </IconContext.Provider>
-              </li>
-            );
-          })}
-        </ul>
+        <h3 className="text-white font-light text-lg underline">Frameworks & Libraries</h3>
+        <Motion>
+          <ul className="flex justify-center flex-wrap gap-5">
+            {techList.frameworks.map((tech) => {
+              return (
+                <li key={uuidv4()}>
+                  <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
+                    <div className="flex flex-col gap-1 items-center">
+                      {tech[0]}
+                      <p className="text-white text-xs">{tech[2]}</p>
+                    </div>
+                  </IconContext.Provider>
+                </li>
+              );
+            })}
+          </ul>
+        </Motion>
       </div>
 
       <hr className="h-[1px] w-full bg-white border-0 my-4" />
 
       <div className="flex flex-col items-center gap-3">
-        <h3 className="text-white font-light text-lg">Skills</h3>
-        <ul className="flex justify-center flex-wrap gap-5">
-          {techList.skills.map((tech) => {
-            return (
-              <li key={uuidv4()}>
-                <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
-                  <div className="flex flex-col gap-1 items-center">
-                    {tech[0]}
-                    <p className="text-white text-xs">{tech[2]}</p>
-                  </div>
-                </IconContext.Provider>
-              </li>
-            );
-          })}
-        </ul>
+        <h3 className="text-white font-light text-lg underline">Skills</h3>
+        <Motion>
+          <ul className="flex justify-center flex-wrap gap-5">
+            {techList.skills.map((tech) => {
+              return (
+                <li key={uuidv4()}>
+                  <IconContext.Provider value={{ color: tech[1], size: '3rem' }}>
+                    <div className="flex flex-col gap-1 items-center">
+                      {tech[0]}
+                      <p className="text-white text-xs">{tech[2]}</p>
+                    </div>
+                  </IconContext.Provider>
+                </li>
+              );
+            })}
+          </ul>
+        </Motion>
       </div>
     </article>
   );
